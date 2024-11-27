21da0b825ba0f91cdfc3bf37fb95543f3642889e
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-27 15:52:35 +0600

    error pages added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,12 @@
 
-
+import { useNavigate } from 'react-router-dom'
+import error from '../assets/images/404/404.gif'
 export default function Error() {
+    const navigate = useNavigate()
     return (
-        <div>
-
+        <div className='flex justify-center items-center flex-col'>
+            <img src={error} alt="" />
+            <button onClick={() => navigate(-1)} className='btn rounded-md shadow-md border-none text-white bg-[#51C668]'>Go Back</button>
         </div>
     )
 }
